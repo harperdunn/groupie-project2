1030cc7a0cb322e452045d2232a29d0a3d02f11d
sdetorres2024 <saradetorres24@gmail.com>
2024-03-08 15:22:52 -0800

    made navbar vertical and left-aligned

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,16 @@
 // components/Layout.js
-// this is the layout component, mainly it format the navbar
+// this is the layout component, mainly it formats the navbar
 // use this wrapper in your page to make a navbar appear there
 import React from 'react';
 import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
     return (
-        <>
+        <div className='page-wrapper'>
             <Navbar />
-            <main>{children}</main>
+            <div className='main-content'>{children}</div>
             {/* Footer */}
-        </>
+        </div>
     );
 };
 
