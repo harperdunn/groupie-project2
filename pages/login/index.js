e6e1b12c17cb1686c67d01244eb44a2f835b017f
harperdunn <harperdunn@ucla.edu>
2024-03-08 12:54:47 -0800

    sign in with google button

--- pages/login/index.js
+++ pages/login/index.js
@@ -16,7 +16,7 @@ const provider= new GoogleAuthProvider();
 const router=useRouter();
 const[user, loading]=useAuthState(auth); //gets the user state, loading to know to wait for it to load or not
 
-const signIn= async () =>{ //calls the popup function with the client and provider given
+const signInWithGoogle= async () =>{ //calls the popup function with the client and provider given
   const result = await signInWithPopup(auth, provider)
   console.log(result.user);
   if (loading){
@@ -35,8 +35,8 @@ return (
     <div className="text-wrapper">GROUPIE</div>
     <div className="signin-container">
       <div>Please sign in to continue...</div>
-      <button className='signin-button' onClick={signIn}>
-        <div>Sign In</div>
+      <button className='signin-button' onClick={signInWithGoogle}>
+        <div>Sign In With Google</div>
       </button>
     </div>
   </div>
