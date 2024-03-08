ae173cea1159954fb4fe4c2fe9706f506398d56e
harperdunn <harperdunn@ucla.edu>
2024-03-08 12:41:54 -0800

    hiding api key

--- firebase.js
+++ firebase.js
@@ -7,19 +7,8 @@ import { getStorage } from "firebase/storage"; // Assuming you're using Firebase
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, onAuthStateChanged} from "firebase/auth";
 import {useEffect, useState} from 'react'
+import firebaseConfig from './env'
 
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCSDO3xcLU-_cQ4jGPR3bh7pUrkmaJQWxA",
-  authDomain: "groupie-fire.firebaseapp.com",
-  projectId: "groupie-fire",
-  storageBucket: "groupie-fire.appspot.com",
-  messagingSenderId: "683212724430",
-  appId: "1:683212724430:web:2b28db074afd44c032989f",
-  measurementId: "G-Z5ETPW62V9"
-};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
