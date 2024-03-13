4a48c6573f6e43932a5b5872486bdba5aac02289
tfirouzabady <156024783+tfirouzabady@users.noreply.github.com>
2024-03-13 16:52:51 -0700

    Update discover.js

--- pages/discover/discover.js
+++ pages/discover/discover.js
@@ -6,69 +6,69 @@ import Layout from '../../components/Layout';
 import './discover.css';
 
 const Discover = () => {
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [refreshKey, setRefreshKey] = useState(0);
-  
-    useEffect(() => {
-      const fetchRandomPosts = async () => {
-        try {
-          const querySnapshot = await getDocs(collection(db, 'posts'));
-          const allPosts = [];
-          querySnapshot.forEach((doc) => {
-            allPosts.push({ id: doc.id, ...doc.data() });
-          });
-          // Shuffle the posts array to get random posts
-          const shuffledPosts = allPosts.sort(() => Math.random() - 0.5);
-          // Get the first 9 posts
-          const randomPosts = shuffledPosts.slice(0, 9);
-          setPosts(randomPosts);
-          setLoading(false);
-        } catch (error) {
-          console.error('Error fetching random posts:', error);
-        }
-      };
-  
-      fetchRandomPosts();
-    }, [refreshKey]);
-  
-    const router = useRouter();
-    const navigateToPost = (postId) => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  useEffect(() => {
+    const fetchRandomPosts = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'posts'));
+        const allPosts = [];
+        querySnapshot.forEach((doc) => {
+          allPosts.push({ id: doc.id, ...doc.data() });
+        });
+        // Shuffle the posts array to get random posts
+        const shuffledPosts = allPosts.sort(() => Math.random() - 0.5);
+        // Get the first 9 posts
+        const randomPosts = shuffledPosts.slice(0, 9);
+        setPosts(randomPosts);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching random posts:', error);
+      }
+    };
+
+    fetchRandomPosts();
+  }, [refreshKey]);
+
+  const router = useRouter();
+  const navigateToPost = (postId) => {
     router.push(`/post/${postId}`);
-    };
-
-    const handleRefresh = () => {
-      setRefreshKey(oldKey => oldKey + 1);
-    };
-
-    return (
-      <Layout>
-        <div className='discover-section'>
-          <h1>Discover</h1>
-          <h2>Want to try something new? Check out some randomized reviews!</h2>
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            <div className="discover-section">
-              <div className="discover-posts-container">
-                {posts.map(({ id, artist, date, venue, rating }) => (
-                  <div key={id} className="discover-post-thumbnail" onClick={() => navigateToPost(id)}>
-                    <h4>{artist}</h4>
-                    <p>Date: {date}</p>
-                    <p>Venue: {venue}</p>
-                    <p>Rating: {rating}/5</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-        </div>
-        <div>
-            <button className='refresh-button' onClick={(event) => handleRefresh()} style={{marginLeft: '10px'}}>Refresh</button>
-        </div>
-      </Layout>
-    );
   };
-  
-  export default Discover;
-  
+
+  const handleRefresh = () => {
+    setRefreshKey(oldKey => oldKey + 1);
+  };
+
+  return (
+    <Layout>
+      <div className='discover-section'>
+        <h1>Discover</h1>
+        <h2>Want to try something new? Check out some randomized reviews!</h2>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="discover-section">
+            <div className="discover-posts-container">
+              {posts.map(({ id, artist, date, venue, rating, likes }) => (
+                <div key={id} className="discover-post-thumbnail" onClick={() => navigateToPost(id)}>
+                  <h4>{artist}</h4>
+                  <p>Date: {date}</p>
+                  <p>Venue: {venue}</p>
+                  <p>Rating: {rating}/5</p>
+                  <p>Likes: {likes.length}</p> {/* Display the like count */}
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+      <div>
+        <button className='refresh-button' onClick={handleRefresh} style={{marginLeft: '10px'}}>Refresh</button>
+      </div>
+    </Layout>
+  );
+};
+
+export default Discover;
